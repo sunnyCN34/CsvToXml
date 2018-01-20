2946209512d8ccca34801f9fbc6d022bb785cee5
sunny sahu <sunny.cn34@gmail.com>
2018-01-20 16:56:15 -0500

    add comments and make the code more readable

--- ExtractWords.java
+++ ExtractWords.java
@@ -1,20 +1,29 @@
 import java.util.*;
 import java.util.Scanner;
 import java.io.File;
-class ExtractWords {
-    public getList() {
-        
-            File f1 = new File("mySample.csv");
-            String xmlOutput= "./xmlOutput.xml";
-            Scanner inputStream = new Scanner(f1);
-            inputStream.useDelimiter(",");
-            String word; 
-            List<String> item = new ArrayList<String>();
-            while (inputStream.hasNext()) {
-                 word = inputStream.next();
-                item.add(word);
 
-                }
-                return item;
-            }
+class ExtractWords {
+
+    //this function returns a list with words
+    public getList() {
+
+        //read the input csv file it should be in the same folder
+        File f1 = new File("mySample.csv");
+
+        Scanner inputStream = new Scanner(f1);
+
+        //use delimiter to separate all words with comma
+        inputStream.useDelimiter(",");
+        String word;
+        List < String > item = new ArrayList < String > ();
+        while (inputStream.hasNext()) {
+            word = inputStream.next();
+
+            //add words to a new list
+            item.add(word);
+
         }
+        //return list and use it in main function to generate XML
+        return item;
+    }
+}
--- src.java
+++ src.java
@@ -2,16 +2,20 @@ import java.util.*;
 import java.util.Scanner;
 import java.io.File;
 import com.thoughtworks.xstream.XStream;
+
 class ConverToXml {
+
     public static void main(String args[]) {
+
+        ExtractWords obj = new ExtractWords();
+        obj.getList();
         
-            ExtractWords obj= new ExtractWords();
-            obj.getList();
-            //String array_of_words[]= item.toArray(new String[0]);
+        //this is the file where output will be writen
+        String xmlOutput = "./xmlOutput.xml";
 
-            XStream xObject= new XStream();
-            xObject.toXML(item, new FileWriter(xmlOutput));
-            }  
-        } 
+        //use xStream library to generate XML
+        XStream xObject = new XStream();
+        xObject.toXML(item, new FileWriter(xmlOutput));
 
-    
+    }
+}
