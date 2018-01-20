4075a778607f33695ce29f5f8e23fda3bfcb87bc
sunny sahu <sunny.cn34@gmail.com>
2018-01-19 19:21:19 -0500

    use xStream library to generate XML output from list

--- src.java
+++ src.java
@@ -1,20 +1,25 @@
 import java.util.*;
 import java.util.Scanner;
 import java.io.File;
+import com.thoughtworks.xstream.XStream;
 class ConverToXml {
     public static void main(String args[]) {
         
             File f1 = new File("mySample.csv");
+            String xmlOutput= "./xmlOutput.xml";
             Scanner inputStream = new Scanner(f1);
             inputStream.useDelimiter(",");
-            String header[]=  
+            String word; 
             List<String> item = new ArrayList<String>();
             while (inputStream.hasNext()) {
-                String word = inputStream.next();
+                 word = inputStream.next();
                 item.add(word);
 
                 }
-            String array_of_words[]= item.toArray(new String[0]);
+            //String array_of_words[]= item.toArray(new String[0]);
+
+            XStream xObject= new XStream();
+            xObject.toXML(item, new FileWriter(xmlOutput));
             }  
         } 
 
