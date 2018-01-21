6176667d65d257d787406eee89244046e915fdd6
sunny sahu <sunny.cn34@gmail.com>
2018-01-20 19:07:39 -0500

    minor change in main function

--- src.java
+++ src.java
@@ -8,14 +8,13 @@ class ConverToXml {
     public static void main(String args[]) {
 
         ExtractWords obj = new ExtractWords();
-        obj.getList();
         
         //this is the file where output will be writen
         String xmlOutput = "./xmlOutput.xml";
 
         //use xStream library to generate XML
         XStream xObject = new XStream();
-        xObject.toXML(item, new FileWriter(xmlOutput));
+        xObject.toXML(obj.getList(), new FileWriter(xmlOutput));
 
     }
 }
